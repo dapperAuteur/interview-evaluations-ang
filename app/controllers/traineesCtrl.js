0d11148bff091221f4a756a1acb13e72db647712
sharangw <sharang.wakankar@hotmail.com>
2017-03-10 14:09:11 -0700

    add find by name functionality

--- app/controllers/traineesCtrl.js
+++ app/controllers/traineesCtrl.js
@@ -1,2 +1,57 @@
 'use strict';
-angular.module('myApp').controller('TraineesCtrl', function($state){});
+angular.module('myApp').controller('TraineesCtrl', function($http){
+	
+  var myData = this;
+
+  myData.getTrainees = function(input1, input2) {
+	  
+	  console.log(input1 + " " + input2);
+	 
+	  if (typeof input1 !== 'undefined' && typeof input2 !== 'undefined') {
+		  
+		    $http({
+		        method: "GET",
+		        url: "//localhost:8080/api/v1/persons?firstname=" + input1 + "&lastname=" +input2
+		        
+		      }).then(function(response){
+		        console.log(response);
+		        myData.trainees = response.data;
+		        console.log("success " + response.data);
+
+		      }, function(response){
+		        console.log("fail " + response);
+		      });
+		  
+	  } else if (typeof input1 !== 'undefined' && typeof input2 === 'undefined') {
+		  
+		  $http({
+		        method: "GET",
+		        url: "//localhost:8080/api/v1/persons?firstname=" + input1
+		        
+		      }).then(function(response){
+		        console.log(response);
+		        myData.trainees = response.data;
+		        console.log("success " + response.data);
+
+		      }, function(response){
+		        console.log("fail " + response);
+		      });
+		  
+	  } else if (typeof input1 === 'undefined' && typeof input2 !== 'undefined') {
+		  
+		  $http({
+		        method: "GET",
+		        url: "//localhost:8080/api/v1/persons?lastname=" + input2
+		        
+		      }).then(function(response){
+		        console.log(response);
+		        myData.trainees = response.data;
+		        console.log("success " + response.data);
+
+		      }, function(response){
+		        console.log("fail " + response);
+		      });
+	  }
+
+  }
+});
