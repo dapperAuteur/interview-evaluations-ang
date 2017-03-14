1898c650ac49221e3fd37c68097f632bf8ef778d
awe <aweful@awews.com>
2017-03-13 19:11:57 -0700

    update readme, remove toggle from project description and add to architecture, add more text to faq

--- app/controllers/faqCtrl.js
+++ app/controllers/faqCtrl.js
@@ -1,13 +1,18 @@
 'use strict';
 angular.module('myApp').controller('FAQCtrl', function($state){
 	var myFaq=this;
-	myFaq.projectDesc = false;
-	
+	myFaq.projectDesc = true;
+
 	myFaq.togProjDesc = function(){
 		myFaq.projectDesc = !myFaq.projectDesc;
 	}
-  
-  
+	myFaq.arch = false;
+
+	myFaq.togArch = function(){
+		myFaq.arch = !myFaq.arch;
+	}
+
+
 });
 
 // var showApp = angular.module('showApp', [])
