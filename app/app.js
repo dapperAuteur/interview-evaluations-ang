a7a900809c31c03514910273752b1320e69a81e5
awe <aweful@awews.com>
2017-03-15 14:21:46 -0700

    remove all references too subject

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ var ie = angular.module('myApp', [
   //internal
   // 'dateDirective'
   // 'HomeCtrl',
-  
+
 
 ]);
   ie.config(function($stateProvider, $urlRouterProvider) {
@@ -22,7 +22,7 @@ var ie = angular.module('myApp', [
         url: '/persons',
         templateUrl: 'app/templates/persons-tmpl.html'
 
-      }) 
+      })
       .state('batch', {
         url: '/batch',
         templateUrl: 'app/templates/batch-tmpl.html',
@@ -31,7 +31,7 @@ var ie = angular.module('myApp', [
       .state('evaluation', {
         url: '/evaluations',
         templateUrl: 'app/templates/evaluations-tmpl.html'
-      })  
+      })
       .state('wiki', {
         url: '/wiki',
         templateUrl: 'app/templates/wiki-tmpl.html',
@@ -41,12 +41,7 @@ var ie = angular.module('myApp', [
         url: '/questions',
         templateUrl: 'app/templates/questions-tmpl.html'
       })
-      .state('subjects', {
-        url: '/subjects',
-        templateUrl: 'app/templates/subjects-tmpl.html',
-        controller: 'SubjectsCtrl as subjects'
-      })
-      
+
     //default routing
     $urlRouterProvider.otherwise('/');
   });
